321348fff5522b700f09f1f36cbbbb265841665d
Paweł K <grylion02@gmail.com>
2023-06-06 16:36:16 +0200

    Open home page test

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -1,13 +1,10 @@
 package tests;
 
-import com.codeborne.selenide.Config;
-import com.codeborne.selenide.Selenide;
-import configuration.ConfigSelenide;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class HomePage extends TestBase{
+public class HomePageTests extends TestBase{
     @Test
     public void openHomePage(){
         open("/");
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,2 +1,13 @@
-package tests;public class TestBase {
+package tests;
+
+import configuration.ConfigSelenide;
+import org.testng.annotations.BeforeClass;
+
+public class TestBase {
+
+    @BeforeClass
+    public void setUp(){
+        ConfigSelenide config = new ConfigSelenide();
+        config.setupConfigForBrowser();
+    }
 }
