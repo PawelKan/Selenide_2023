c8c0f78450cdda1b65e5c711dc1b6d00f19466a2
Paweł K <grylion02@gmail.com>
2023-06-14 16:38:00 +0200

    Add veryfication for user name after successfull login.

--- src/test/java/pageObjects/HomePageObj.java
+++ src/test/java/pageObjects/HomePageObj.java
@@ -3,7 +3,7 @@ package pageObjects;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Autowired;
+import testData.Users;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,10 +14,16 @@ public class HomePageObj {
     private SelenideElement sectionHeader = $("#header");
     private SelenideElement sectionContent = $("body > section:nth-child(3)");
     private SelenideElement sectionFooter = $("#footer");
+    private SelenideElement header_LoggedInUserName = $("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(10) > a");
+    private String textLoggedInUser = " Logged in as ";
 
     public void verifySectionHeader(){ getSectionHeader().shouldBe(Condition.visible); }
     public void verifySectionContent(){ getSectionContent().shouldBe(Condition.visible); }
     public void verifySectionFooter(){ getSectionFooter().shouldBe(Condition.visible); }
+    public void verifyUserNameIsVisibleInHeaderAfterLogin(String userName){
+        header_LoggedInUserName.shouldHave(Condition.text(textLoggedInUser));
+        header_LoggedInUserName.shouldHave(Condition.text(userName));
+    }
 
 
 
--- src/test/java/tests/LoginPageTests.java
+++ src/test/java/tests/LoginPageTests.java
@@ -1,7 +1,10 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pageObjects.HomePageObj;
 import pageObjects.LoginPageObj;
 import testData.Users;
 import utils.Urls;
@@ -10,7 +13,9 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class LoginPageTests extends TestBase{
 
+    HomePageObj homePage = new HomePageObj();
     LoginPageObj loginPage = new LoginPageObj();
+
 
     @BeforeMethod
     public void openLoginPage(){ open(Urls.LOGIN_PAGE);    }
@@ -22,5 +27,6 @@ public class LoginPageTests extends TestBase{
     @Test
     public void verifyUserIsAbleToLoginIntoApp(){
         loginPage.loginAsUser(Users.VALID_USER_EMAIL, Users.VALID_USER_PASS);
+        homePage.verifyUserNameIsVisibleInHeaderAfterLogin(Users.VALID_USER_NAME);
     }
 }
